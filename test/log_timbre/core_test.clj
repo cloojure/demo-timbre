c1c9e31235983e3d239eb08c2ad01848e2fb1319
Alan Thompson <cloojure@gmail.com>
2014-10-31 11:17:36 -0700

    misc

--- test/log_timbre/core_test.clj
+++ test/log_timbre/core_test.clj
@@ -1,7 +1,13 @@
 (ns log-timbre.core-test
   (:require [clojure.test :refer :all]
-            [log-timbre.core :refer :all]))
+            [log-timbre.core :refer :all]
+            [taoensso.timbre :as timbre] ))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "dummy test"
+    (is (= 1 1))
+
+    ; Demonstrate logging with Timbre
+    (timbre/info "Dummy test passed.")
+    (println     "Dummy test passed.")
+  ))
